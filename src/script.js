88aa75328a015560b575976eb73f720ed2e88489
Satyam Verma <satyamverma46@outlook.com>
2022-12-11 20:44:08 +0530

    Initial Commit

---
+++ src/script.js
@@ -0,0 +1,90 @@
+var _topicOneNext = document.querySelector('.next1');
+var _topicTwoNext = document.querySelector('.next2');
+var _topicThreeNext = document.querySelector('.next3');
+var _topicFourNext = document.querySelector('.next4');
+var _topicFiveNext = document.querySelector('.next5');
+/*var _topicSixNext = document.querySelector('.next6');
+var _topicSevenNext = document.querySelector('.next7');
+var _topicEightNext = document.querySelector('.next8');
+var _topicNineNext = document.querySelector('.next9');
+var _topicTenNext = document.querySelector('.next10');
+var _topicElevenNext = document.querySelector('.next11');
+var _topicTwelveNext = document.querySelector('.next12');*/
+
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+function onStart() {
+    _topicOneNext.style.display = 'block';
+    _topicTwoNext.style.display = 'none';
+    _topicThreeNext.style.display = 'none';
+    _topicFourNext.style.display = 'none';
+    _topicFiveNext.style.display = 'none';
+    /*_topicSixNext.style.display = 'none';
+    _topicSevenNext.style.display = 'none';
+    _topicEightNext.style.display = 'none';
+    _topicNineNext.style.display = 'none';
+    _topicTenNext.style.display = 'none';
+    _topicElevenNext.style.display = 'none';
+    _topicTwelveNext.style.display = 'none';*/
+}
+
+function onClickOneNext() {
+    _topicOneNext.style.display = 'none';
+    _topicTwoNext.style.display = 'block';
+    _topicThreeNext.style.display = 'none';
+    _topicFourNext.style.display = 'none';
+    _topicFiveNext.style.display = 'none';
+    /*_topicSixNext.style.display = 'none';
+    _topicSevenNext.style.display = 'none';
+    _topicEightNext.style.display = 'none';
+    _topicNineNext.style.display = 'none';
+    _topicTenNext.style.display = 'none';
+    _topicElevenNext.style.display = 'none';
+    _topicTwelveNext.style.display = 'none';*/
+}
+function onClickTwoNext() {
+    _topicOneNext.style.display = 'none';
+    _topicTwoNext.style.display = 'none';
+    _topicThreeNext.style.display = 'block';
+    _topicFourNext.style.display = 'none';
+    _topicFiveNext.style.display = 'none';
+    /*_topicSixNext.style.display = 'none';
+    _topicSevenNext.style.display = 'none';
+    _topicEightNext.style.display = 'none';
+    _topicNineNext.style.display = 'none';
+    _topicTenNext.style.display = 'none';
+    _topicElevenNext.style.display = 'none';
+    _topicTwelveNext.style.display = 'none';*/
+}
+
+function onClickThreeNext() {
+    _topicOneNext.style.display = 'none';
+    _topicTwoNext.style.display = 'none';
+    _topicThreeNext.style.display = 'none';
+    _topicFourNext.style.display = 'block';
+    _topicFiveNext.style.display = 'none';
+    /*_topicSixNext.style.display = 'none';
+    _topicSevenNext.style.display = 'none';
+    _topicEightNext.style.display = 'none';
+    _topicNineNext.style.display = 'none';
+    _topicTenNext.style.display = 'none';
+    _topicElevenNext.style.display = 'none';
+    _topicTwelveNext.style.display = 'none';*/
+}
+
+function onClickFourNext() {
+    _topicOneNext.style.display = 'none';
+    _topicTwoNext.style.display = 'none';
+    _topicThreeNext.style.display = 'none';
+    _topicFourNext.style.display = 'none';
+    _topicFiveNext.style.display = 'block';
+    /*_topicSixNext.style.display = 'none';
+    _topicSevenNext.style.display = 'none';
+    _topicEightNext.style.display = 'none';
+    _topicNineNext.style.display = 'none';
+    _topicTenNext.style.display = 'none';
+    _topicElevenNext.style.display = 'none';
+    _topicTwelveNext.style.display = 'none';*/
+}
