112c3559d37d4cc16aff11ba6621e52160548e6a
Satyam Verma <satyamverma46@outlook.com>
2022-12-18 17:28:36 +0530

    modified:   .gitignore new file:   assets/images/_img_08.webp modified:   index.html modified:   src/script.js modified:   src/style.css

--- src/script.js
+++ src/script.js
@@ -3,8 +3,8 @@ var _topicTwoNext = document.querySelector('.next2');
 var _topicThreeNext = document.querySelector('.next3');
 var _topicFourNext = document.querySelector('.next4');
 var _topicFiveNext = document.querySelector('.next5');
-/*var _topicSixNext = document.querySelector('.next6');
-var _topicSevenNext = document.querySelector('.next7');
+var _topicSixNext = document.querySelector('.next6');
+/*var _topicSevenNext = document.querySelector('.next7');
 var _topicEightNext = document.querySelector('.next8');
 var _topicNineNext = document.querySelector('.next9');
 var _topicTenNext = document.querySelector('.next10');
@@ -21,8 +21,8 @@ function onStart() {
     _topicThreeNext.style.display = 'none';
     _topicFourNext.style.display = 'none';
     _topicFiveNext.style.display = 'none';
-    /*_topicSixNext.style.display = 'none';
-    _topicSevenNext.style.display = 'none';
+    _topicSixNext.style.display = 'none';
+/*    _topicSevenNext.style.display = 'none';
     _topicEightNext.style.display = 'none';
     _topicNineNext.style.display = 'none';
     _topicTenNext.style.display = 'none';
@@ -36,8 +36,8 @@ function onClickOneNext() {
     _topicThreeNext.style.display = 'none';
     _topicFourNext.style.display = 'none';
     _topicFiveNext.style.display = 'none';
-    /*_topicSixNext.style.display = 'none';
-    _topicSevenNext.style.display = 'none';
+    _topicSixNext.style.display = 'none';
+/*    _topicSevenNext.style.display = 'none';
     _topicEightNext.style.display = 'none';
     _topicNineNext.style.display = 'none';
     _topicTenNext.style.display = 'none';
@@ -50,8 +50,8 @@ function onClickTwoNext() {
     _topicThreeNext.style.display = 'block';
     _topicFourNext.style.display = 'none';
     _topicFiveNext.style.display = 'none';
-    /*_topicSixNext.style.display = 'none';
-    _topicSevenNext.style.display = 'none';
+    _topicSixNext.style.display = 'none';
+/*    _topicSevenNext.style.display = 'none';
     _topicEightNext.style.display = 'none';
     _topicNineNext.style.display = 'none';
     _topicTenNext.style.display = 'none';
@@ -65,8 +65,8 @@ function onClickThreeNext() {
     _topicThreeNext.style.display = 'none';
     _topicFourNext.style.display = 'block';
     _topicFiveNext.style.display = 'none';
-    /*_topicSixNext.style.display = 'none';
-    _topicSevenNext.style.display = 'none';
+    _topicSixNext.style.display = 'none';
+/*    _topicSevenNext.style.display = 'none';
     _topicEightNext.style.display = 'none';
     _topicNineNext.style.display = 'none';
     _topicTenNext.style.display = 'none';
@@ -80,8 +80,23 @@ function onClickFourNext() {
     _topicThreeNext.style.display = 'none';
     _topicFourNext.style.display = 'none';
     _topicFiveNext.style.display = 'block';
-    /*_topicSixNext.style.display = 'none';
-    _topicSevenNext.style.display = 'none';
+    _topicSixNext.style.display = 'none';
+/*    _topicSevenNext.style.display = 'none';
+    _topicEightNext.style.display = 'none';
+    _topicNineNext.style.display = 'none';
+    _topicTenNext.style.display = 'none';
+    _topicElevenNext.style.display = 'none';
+    _topicTwelveNext.style.display = 'none';*/
+}
+
+function onClickFiveNext() {
+    _topicOneNext.style.display = 'none';
+    _topicTwoNext.style.display = 'none';
+    _topicThreeNext.style.display = 'none';
+    _topicFourNext.style.display = 'none';
+    _topicFiveNext.style.display = 'none';
+    _topicSixNext.style.display = 'block';
+/*    _topicSevenNext.style.display = 'none';
     _topicEightNext.style.display = 'none';
     _topicNineNext.style.display = 'none';
     _topicTenNext.style.display = 'none';
